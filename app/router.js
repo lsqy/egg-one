a60ef60619bfa511d7df53edeb447e1c2dada4de
lsqy <lsqy93@gmail.com>
2017-12-13 19:38:21 +0800

    GFix

--- app/router.js
+++ app/router.js
@@ -13,6 +13,9 @@ module.exports = app => {
   router.get('/one/music', controller.one.musicList);
 
   //api
-  router.resources('music', '/api/v1/music', app.controller.music);
-  router.resources('musicDetail', '/api/v1/musicDetail', app.controller.musicDetail);
+  router.resources('music', '/api/v1/music', controller.music);
+  router.resources('musicDetail', '/api/v1/musicDetail', controller.musicDetail);
+  router.resources('idlist', '/api/v1/onelist/idlist', controller.idList);
+  // router.resources('onelist', '/api/v1/onelist', app.controller.oneList);
+  router.get('/api/v1/onelist/:id/:index', controller.oneList.show);
 };
---
+++ app/service/OneList.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+/**
+ * OneList Api Service
+ */
+class OneList extends Service {
+  constructor(ctx) {
+    super(ctx);
+    this.config = this.ctx.app.config.one;
+    this.serverUrl = this.config.serverUrl;
+    this.pageSize = this.config.pageSize;
+  }
+
+  /**
+   * request one api
+   * @param {String} api - Api name
+   * @param {Object} [opts] - urllib options
+   * @return {Promise} response.data
+   */
+  async request(api, opts) {
+    const options = Object.assign({
+      dataType: 'json',
+      timeout: [ '30s', '30s' ],
+    }, opts);
+    const result = await this.ctx.curl(`${this.serverUrl}/${api}`, options);
+    return result.data;
+  }
+
+  // 获取图文列表索引
+  async getOneListIndex(params) {
+    let url = 'onelist/idlist';
+    const result = await this.request(url, {
+    });
+    return result;
+  }
+
+  // 根据索引获取图文详情
+  async getOneListItem(params) {
+    const { id, index } = params;
+    let url = `onelist/${id}/${index}`;
+    console.log(url);
+    const result = await this.request(url, {
+    });
+    return result;
+  }
+
+
+
+  checkSuccess(result) {
+    if (result.status !== 200) {
+      const errorMsg = result.data && result.data.error_msg ? result.data.error_msg : 'unknown error';
+      this.ctx.throw(result.status, errorMsg);
+    }
+    if (!result.data.success) {
+      this.ctx.throw(500, 'remote response error', { data: result.data });
+    }
+  }
+}
+
+module.exports = OneList;
