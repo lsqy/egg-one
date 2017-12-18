c1f1488bac7237a9a298c598224945eaf8a67f20
lsqy <lsqy93@gmail.com>
2017-12-18 19:40:11 +0800

    Fix

--- app/router.js
+++ app/router.js
@@ -27,4 +27,8 @@ module.exports = app => {
   router.get('/api/v1/reading/:id', controller.reading.getList);
   // 请求阅读详情
   // router.get('/api/v1/reading/detail/:id', controller.music.getDetail);
+  // 请求影视列表
+  router.get('/api/v1/movie/:id', controller.movie.getList);
+  // 请求影视详情
+  router.get('/api/v1/movie/detail/:id', controller.movie.getDetail);
 };
