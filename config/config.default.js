4288a9c531c9e2e2043288e673a19ffe9a5b2d46
lsqy <lsqy93@gmail.com>
2018-07-26 18:55:18 +0800

    添加mysql支持

--- config/config.default.js
+++ config/config.default.js
@@ -1,36 +1,47 @@
-
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
 
 module.exports = appInfo => {
-  const config = {};
+    const config = {};
 
-  // should change to your own
-  config.keys = appInfo.name + '123456';
+    // should change to your own
+    config.keys = appInfo.name + '123456';
 
-  config.siteFile = {
-    // '/favicon.ico': fs.readFileSync(path.join(appInfo.baseDir, 'app/public/favicon.png')),
-  };
+    config.siteFile = {
+        // '/favicon.ico': fs.readFileSync(path.join(appInfo.baseDir, 'app/public/favicon.png')),
+    };
 
-  config.news = {
-    pageSize: 30,
-    serverUrl: 'http://hacker-news.firebaseio.com/v0',
-  };
+    config.news = {
+        pageSize: 30,
+        serverUrl: 'http://hacker-news.firebaseio.com/v0',
+    };
 
-  config.view = {
-    defaultViewEngine: 'nunjucks',
-    mapping: {
-      '.tpl': 'nunjucks',
-      '.nj': 'nunjucks',
-    },
-  };
+    config.view = {
+        defaultViewEngine: 'nunjucks',
+        mapping: {
+            '.tpl': 'nunjucks',
+            '.nj': 'nunjucks',
+        },
+    };
 
-  config.one = {
-    pageSize: 15,
-    serverUrl: 'http://v3.wufazhuce.com:8000/api',
-  }
+    config.one = {
+        pageSize: 15,
+        serverUrl: 'http://v3.wufazhuce.com:8000/api',
+    };
 
-  return config;
+    config.mysql = {
+        client: {
+            host: 'localhost',
+            port: '3306',
+            user: 'root',
+            password: 'qian1993',
+            database: 'one',
+        },
+        app: true,
+        agent: false,
+    };
+
+    return config;
 };
--- config/plugin.js
+++ config/plugin.js
@@ -9,5 +9,10 @@ exports.validate = {
     enable: true,
     package: 'egg-validate'
 }
+
+exports.mysql = {
+    enable: true,
+    package: 'egg-mysql',
+}
 // had enabled by egg
 // exports.static = true;
